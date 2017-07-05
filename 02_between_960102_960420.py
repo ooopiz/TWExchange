29c1080aded235a77d6c813f6d9c946547d68239
Rickyis <r.huang850@gmail.com>
2017-07-05 09:41:53 +0800

    Add script between 960102 and 960420

---
+++ 02_between_960102_960420.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+import os
+import time
+
+
+def main():
+    this_path = os.path.abspath(os.path.dirname(__file__))
+    crawler_file = os.path.join(this_path, "otc_stocks_daily.py")
+
+    start_date = datetime.datetime(2006, 1, 1)
+    end_date = datetime.datetime(2006, 4, 20)
+
+    dCrawler = start_date
+    while start_date <= end_date:
+        str_day = '{0} {1:02d} {2:02d}'.format(dCrawler.year, dCrawler.month, dCrawler.day)
+        command = 'python3 ' + crawler_file + ' ' + str_day
+        os.system(command)
+        dCrawler = dCrawler + datetime.timedelta(days=1)
+        time.sleep(30)
+
+
+if __name__ == '__main__':
+    main()
